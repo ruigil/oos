485b64232b594b484a4b332a934301a372abacd8
Rui Gil <rui.gil@vd.ch>
2019-01-30 10:17:16 +0100

    Environment directory

---
+++ client/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDoiYT-2KLb9Gi8s0S7lIjJQNlqu6_pwcc",
+    authDomain: "oceanos-163619.firebaseapp.com",
+    databaseURL: "https://oceanos-163619.firebaseio.com",
+    projectId: "oceanos-163619",
+    storageBucket: "oceanos-163619.appspot.com",
+    messagingSenderId: "157531295065"      
+  }
+};
