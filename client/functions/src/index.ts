79f157ee64f2104e4f544d6ae78a1cb8f3cabf3d
Rui Gil <rui@gil.com>
2019-03-29 00:47:20 +0100

    Cloud Function Analytics

--- client/functions/src/index.ts
+++ client/functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+//import { addDays } from 'date-fns';
 
 admin.initializeApp(functions.config().firebase);
 
@@ -9,6 +10,97 @@ admin.initializeApp(functions.config().firebase);
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+async function getAnalytics(month:number,year:number) {
+    var doc = await admin.firestore().doc("analytics/"+month+year).get();
+    console.log("await done.");
+    if (doc.exists) { 
+        console.log("return doc.");
+        return doc.data();
+    } else {
+        return Promise.resolve({ totals: [0,0,0], days: [  ], tags: [ ] } );
+    }
+}
+
+export const updateStats = functions.firestore
+    .document('drops/{dropID}')
+    .onWrite((change, context) => {
+
+        return admin.firestore().doc("drops/aCag1iLYarTc1NlGcm6z").get().then(d => {
+            var dr = d.data();
+            var date = dr ? dr.date.toDate() : new Date();
+
+            var day = date.getDay();
+            var month = Number(date.getMonth());
+            var year = Number(date.getFullYear());
+
+            console.log(month);
+            console.log(year);
+
+            getAnalytics(month,year).then( (a) => {
+                console.log("doing a");
+                console.log(a);
+                var type = 0;
+                switch(dr ? dr.type: "NOTE") {
+                    case "NOTE": type = 0; break;
+                    case "TASK": type = 1; break;
+                    case "TRX": type = 2; break;
+                }
+                type Days = { day: number, totals: Array<number> }
+                type Tags = { tag: string, totals: Array<number> }
+                var tot = [0,0,0].splice(0,3,1);
+                console.log(tot);
+                if (a) {
+                    a.totals[type]++;
+                    // update days structure
+                    var dayExist:boolean  = a.days.filter( (d:Days) => d.day == 1).length != 0;
+                    if (dayExist) {
+                        a.days = a.days.map( (d:Days) => d.day == day ? { day: day, totals: d.totals.map((v,i) =>  i === type ? ++v : v) } : d )
+                    } else a.days.push( { day: day, totals: [0,0,0].map((v,i) =>  i === type ? ++v : v)});
+                    // update tag structure
+                    if (dr) dr.tags.map( (tag:string) => {
+                        var tagExist:boolean = a.tags.filter( (t:Tags) => t.tag === tag).length != 0;
+                        if (tagExist) {
+                            a.tags = a.tags.map( (t:Tags) => t.tag === tag ? { tag: tag, totals: t.totals.map((v,i) =>  i === type ? ++v : v) } : d )
+                        } else a.tags.push( { tag: tag, totals: [0,0,0].map((v,i) =>  i === type ? ++v : v) } );
+                    }); 
+
+                    admin.firestore().doc("analytics/"+month+year).set(a);
+                console.log(a);
+                console.log(a.days);
+                console.log(a.tags);
+                }               
+            });
+
+
+            /**
+                get the month and the year and the day
+                see if there is a analytics bucket for This
+                if there is update it ifnot create one
+
+                update the totals by type
+                update the tags map with each tag of the drop and the type 
+                update the day map with the day and type 
+             */
+        });
+    });
+
+// updateStats({ before: {foo: "old"}, after: {foo: "new"} })
+
+/**
+
+{ 
+    text: 'This is a minute drop...',
+    date: Timestamp { _seconds: 1553202600, _nanoseconds: 0 },
+    recurrence: 'none',
+    tags: [ 'Project', 'AAA' ],
+    completed: null,
+    createdAt: Timestamp { _seconds: 1553202645, _nanoseconds: 192000000 },
+    updatedAt: Timestamp { _seconds: 1553276303, _nanoseconds: 822000000 }
+}
+
+*/
 
 export const timeTrigger = functions.pubsub.topic("oos-time").onPublish(async message => {
     console.log(Buffer.from(message.data,'base64').toString());
