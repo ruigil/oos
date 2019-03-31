cb3fec175db4fea414e5c38f2a5c79fb9dde302b
Rui Gil <rui@gil.com>
2019-03-31 23:50:56 +0200

    UpdateTags CF

--- client/functions/src/index.ts
+++ client/functions/src/index.ts
@@ -11,6 +11,9 @@ admin.initializeApp(functions.config().firebase);
 //  response.send("Hello from Firebase!");
 // });
 
+type Days = { day: number, totals: Array<number> }
+type Tags = { tag: string, totals: Array<number> }
+type Drop = { date: Date, tags: Array<string>, type: string }
 
 async function getAnalytics(month:number,year:number) {
     var doc = await admin.firestore().doc("analytics/"+year+"-"+month).get();
@@ -21,60 +24,108 @@ async function getAnalytics(month:number,year:number) {
         return Promise.resolve({ month: month, year: year, totals: [0,0,0], days: [  ], tags: [ ] } );
 }
 
-type Days = { day: number, totals: Array<number> }
-type Tags = { tag: string, totals: Array<number> }
-type Drop = { date: Date, tags: Array<string>, type: string }
+const incType = function(totals: Array<number>, t:number) { return totals.map((v,i) =>  i === t ? ++v : v) }
+const decType = function(totals: Array<number>, t:number) { return totals.map((v,i) =>  i === t ? --v : v) }
 
-export const updateStats = functions.firestore
+const incDayType = function(days:Array<Days>, day:number, type:number) {
+    var dayExist:boolean  = days.filter( (d:Days) => d.day == day).length != 0;
+
+    if (dayExist) {
+        days = days.map( (d:Days) => d.day == day ? { day: day, totals: incType(d.totals,type) } : d )
+    } else 
+        days.push( { day: day, totals: incType([0,0,0],type)});
+    
+    return days;
+}
+
+const incTagType = function(tags:Array<Tags>, tag:string, type:number) {
+    var tagExist:boolean = tags.filter( (t:Tags) => t.tag === tag ).length != 0;
+
+    if (tagExist) {
+        tags = tags.map( (t:Tags) => t.tag === tag ? { tag: tag, totals: incType(t.totals,type) } : t )
+    } else 
+        tags.push( { tag: tag, totals: incType([0,0,0],type) } );
+    
+    return tags;
+}
+
+const decDayType = function(days:Array<Days>, day:number, type:number) {
+    days = days.map( (d:Days) => d.day == day ? { day: day, totals: decType(d.totals,type) } : d );
+    days = days.filter( (d:Days) => !d.totals.every( v => v == 0) );
+    return days;
+}
+
+const decTagType = function(tags:Array<Tags>, tag:string, type:number) {
+    tags = tags.map( (t:Tags) => t.tag == tag ? { tag: tag, totals: decType(t.totals,type) } : t );
+    tags = tags.filter( (t:Tags) => !t.totals.every( v => v == 0) );
+    return tags;
+}
+
+const getTypeDrop = function(drop:Drop):number {
+    var type:number = 0;
+    switch(drop.type) {
+        case "NOTE": type = 0; break;
+        case "TASK": type = 1; break;
+        case "TRX": type = 2; break;
+    }
+    return type;
+}
+
+export const statsCreate = functions.firestore
     .document('drops/{dropID}')
-    .onWrite((change, context) => {
+    .onCreate((snap, context) => {
+        //const sdrop = snap.data();
+        const drop:Drop = { date: new Date(), tags: ["AAA", "CCC"], type:"TRX" }
 
-        var drop:Drop = { date: new Date(), tags: ["AAA", "CCC"], type:"TRX" }
-        console.log(drop.date);
         var day:number = drop.date.getDate();
         var month:number = drop.date.getMonth() + 1;
         var year:number = drop.date.getFullYear();
 
         getAnalytics(month,year).then( a => {
 
-/**
-if new add date and tag
-if delete substract date and tag
+            const type:number = getTypeDrop(drop);
 
-if updated
- if date changed subtract before date, add new Date
- if tags changed substract removed tags, add new tags
- */
-
-            var type:number = 0;
-            switch(drop.type) {
-                case "NOTE": type = 0; break;
-                case "TASK": type = 1; break;
-                case "TRX": type = 2; break;
-            }
-
-            const addType = function(totals: Array<number>, t:number){ return totals.map((v,i) =>  i === t ? ++v : v)}
             if (a) {
                 a.totals[type]++;
                 // update days structure
-                var dayExist:boolean  = a.days.filter( (d:Days) => d.day == day).length != 0;
-                
-                if (dayExist) {
-                    a.days = a.days.map( (d:Days) => d.day == day ? { day: day, totals: addType(d.totals,type) } : d )
-                } else 
-                    a.days.push( { day: day, totals: addType([0,0,0],type)});
-
+                a.days = incDayType(a.days, day, type)
                 // update tag structure
                 drop.tags.map( (tag:string) => {
-                    var tagExist:boolean = a.tags.filter( (t:Tags) => t.tag === tag).length != 0;
-                    
-                    if (tagExist) {
-                        a.tags = a.tags.map( (t:Tags) => t.tag === tag ? { tag: tag, totals: addType(t.totals,type) } : t )
-                    } else 
-                        a.tags.push( { tag: tag, totals: addType([0,0,0],type) } );
-                }); 
+                    a.tags = incTagType(a.tags, tag, type)
+                });
+                console.log("stats create")
+                console.log(a.totals);
+                console.log(a.days);
+                console.log(a.tags);
+                admin.firestore().doc("analytics/"+year+"-"+month).set(a);
+            }
+        });
+        return drop;
+    });
 
-                console.log(a);
+export const statsDelete = functions.firestore
+    .document('drops/{dropID}')
+    .onCreate((snap, context) => {
+        //const sdrop = snap.data();
+        const drop:Drop = { date: new Date(), tags: ["AAA", "CCC"], type:"TRX" }
+
+        var day:number = drop.date.getDate();
+        var month:number = drop.date.getMonth() + 1;
+        var year:number = drop.date.getFullYear();
+
+        getAnalytics(month,year).then( a => {
+            const type:number = getTypeDrop(drop);
+
+            if (a) {
+                a.totals[type]--;
+                // update days structure
+                a.days = decDayType(a.days, day, type)
+                // update tag structure
+                drop.tags.map( (tag:string) => {
+                    a.tags = decTagType(a.tags, tag, type)
+                });
+                console.log("stats delete");
+                console.log(a.totals);
                 console.log(a.days);
                 console.log(a.tags);
                 admin.firestore().doc("analytics/"+year+"-"+month).set(a);
@@ -117,29 +168,26 @@ export const timeTrigger = functions.pubsub.topic("oos-time").onPublish(async me
 export const updateTags = functions.firestore
     .document('drops/{dropID}')
     .onWrite((change, context) => {
-      // Get an object with the current document value.
-      // If the document does not exist, it has been deleted.
-      let document = null;
-      let oldDocument = null;
+      let newDoc = change.after ? change.after.exists ? change.after.data() : null : null;
+      let oldDoc = change.before ? change.before.exists ? change.before.data() : null : null;
+
+      let newTags:Array<string> =  newDoc ? newDoc.tags : [];
+      let oldTags:Array<string> = oldDoc ? oldDoc.tags : [];
+
+      // increment those that are in the new and not in the old
+      let inc = newTags.filter( nt => oldTags.every( ot => ot !== nt ) )
+      let incTags = inc.map( t => admin.firestore().doc("tags/"+t).get() );
+      Promise.all(incTags).then( docs => {
+          docs.map( d => d.data() ).map( (t:any) => admin.firestore().doc("tags/"+t.name).update({ count: ++t.count, updatedAt: admin.firestore.FieldValue.serverTimestamp()}) )
+      });
       
-      if (change.after) document = change.after.data();
-      if (change.before) oldDocument = change.before.data();
+      // decrement those that are in the old and not in the new
+      let dec = oldTags.filter( ot => newTags.every( nt => nt !== ot ) );
+      let decTags = dec.map( t => admin.firestore().doc("tags/"+t).get() );
+      Promise.all(decTags).then( docs => {
+          docs.map( d => d.data() ).map( (t:any) => admin.firestore().doc("tags/"+t.name).update({ count: --t.count}) )
+      });
 
-      let newTags = document ? document.tags : [];
-      let oldTags = oldDocument ? oldDocument.tags : [];
+      return 0;
 
-      return admin.firestore().collection("tags")
-      .get()
-      .then( querySnapshot => {
-                querySnapshot.forEach(doc => {
-                    const data = doc.data();
-                    if (newTags.includes(data.name) && !oldTags.includes(data.name))
-                        return admin.firestore().doc('tags/'+doc.id).update({ count: data.count + 1, updatedAt: admin.firestore.FieldValue.serverTimestamp() })
-                    if (!newTags.includes(data.name) && oldTags.includes(data.name))
-                        return admin.firestore().doc('tags/'+doc.id).update({ count: data.count - 1 })
-                    return null;
-                });
-      }).catch( error => console.log(error) );
-
-      // perform desired operations ...
     });
