398d02eda15528e066b24321e56feae08d6ba0c0
Rui Gil <rui@gil.com>
2020-03-02 16:43:00 +0000

    Environement files

---
+++ client/src/environments/environment.ts
@@ -0,0 +1,16 @@
+
+export const environment = {
+    production: false,
+    // configure this with your own credentials
+    /*
+    firebase: {
+        apiKey: "your credentials",
+        authDomain: ""your credentials"",
+        databaseURL: ""your credentials"",
+        projectId: ""your credentials"",
+        storageBucket: "",
+        messagingSenderId: ""your credentials"",
+        appId: ""your credentials""
+    }
+    */
+};
