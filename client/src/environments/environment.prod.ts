5732e952496f506f18ea77ee14cbfc139f9d158d
Rui Gil <rui@gil.com>
2020-03-02 17:36:17 +0000

    remove ng

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,15 +2,13 @@
 export const environment = {
     production: true,
     // configure this with your own credentials
-    /*
     firebase: {
         apiKey: "your credentials",
-        authDomain: ""your credentials"",
-        databaseURL: ""your credentials"",
-        projectId: ""your credentials"",
+        authDomain: "your credentials",
+        databaseURL: "your credentials",
+        projectId: "your credentials",
         storageBucket: "",
-        messagingSenderId: ""your credentials"",
-        appId: ""your credentials""
+        messagingSenderId: "your credentials",
+        appId: "your credentials"
     }
-    */
 };
