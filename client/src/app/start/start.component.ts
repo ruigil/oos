a54cbad622483181d8a33db710162b65057f6ccf
Rui Gil <rui@gil.com>
2022-08-10 19:16:50 +0000

    component for public drops

--- client/src/app/start/start.component.ts
+++ client/src/app/start/start.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
 
 @Component({
-  selector: 'app-start',
+  selector: 'oos-start',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.scss']
 })
 export class StartComponent implements OnInit {
+  @ViewChild(MatAccordion) accordion: MatAccordion = new MatAccordion();
+  drops: Array<number> = [...Array(1).keys()];
+  constructor() { 
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
